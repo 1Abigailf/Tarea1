5612b8822b3c6e27f96428c4b0d95ca75c70e8ba
Tecnova <Tecnova@ANDXZEL>
2023-02-18 15:28:07 -0600

    Agregar archivos de proyecto.

---
+++ Tarea1/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tarea1
+{
+      public partial class MainPage : ContentPage
+    {
+        int numero1;
+        int numero2;
+        double operationr ;
+    
+        
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+    
+
+        private void btnsumar_Clicked(object sender, EventArgs e)
+        {
+            
+           
+            numero1 = int.Parse(txtn1.Text);
+            numero2 = int.Parse(txtn2.Text);
+            operationr = numero1 + numero2;
+            lblesltado.Text = operationr + "";
+        }
+
+        private void btnrestar_Clicked(object sender,EventArgs e)
+        {
+            numero1 = int.Parse(txtn1.Text);
+            numero2 = int.Parse(txtn2.Text);
+            operationr = numero1 - numero2;
+            lblesltado.Text = operationr + "";
+        }
+
+        private void btnmultiplicar_Clicked(object sender, EventArgs e)
+        {
+            numero1 = int.Parse(txtn1.Text);
+            numero2 = int.Parse(txtn2.Text);
+            operationr = numero1 * numero2;
+            lblesltado.Text = operationr + "";
+        }
+
+        private void btndividir_Clicked(object sender, EventArgs e)
+        {
+            numero1 = int.Parse(txtn1.Text);
+            numero2 = int.Parse(txtn2.Text);
+            operationr = numero1 / numero2;
+            lblesltado.Text = operationr + "";
+        }
+         
+
+    }
+}
